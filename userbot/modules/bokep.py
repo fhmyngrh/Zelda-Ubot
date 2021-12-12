d1df810a395e19d33033198f857604e855ef1be5
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-12 18:00:11 +0700

    update bokep.py

--- userbot/modules/bokep.py
+++ userbot/modules/bokep.py
@@ -20,7 +20,7 @@ async def amireallysxs(sxs):
     await sxs.edit("⚡")
     await asyncio.sleep(2)
     output = (
-        f"**{capti}**\n\n"
+        f"**{capti}\n\n"
         f"⬇️ KLIK UNTUK MENONTON ⬇️\n"
         f"{link}\n\n"
         f"📍Support Join : @LustsketchID\n"
@@ -42,6 +42,47 @@ async def amireallysxs(sxs):
             # await sxs.delete()
     else:
         await edit_or_reply(sxs, output)
+        
+        
+@zelda_cmd(pattern="lsid (.*)")
+async def amireallylsid(lsid):
+    user = await bot.get_me()
+    capt = str(lsid.pattern_match.group(1).split(" ", 2)[0])
+    link = str(lsid.pattern_match.group(1).split(" ", 2)[1])
+    capti = capt.replace("_", " ")
+    thumb = "https://telegra.ph/file/22eda4c6851fd81b3b46a.jpg"
+    await lsid.edit("__Please Wait.__")
+    await lsid.edit("__Please Wait..__")
+    await lsid.edit("__Please Wait.__")
+    await lsid.edit("__Please Wait..__")
+    await lsid.edit("__Creating Content...__")
+    await lsid.edit("__Creating Content..__")
+    await lsid.edit("__Creating Content...__")
+    await lsid.edit("⚡")
+    await asyncio.sleep(2)
+    output = (
+        f"**{capti}\n\n"
+        f"⬇️ Your Link\n"
+        f"{link}\n\n"
+        f"📍Support Join : @SexualSins58\n"
+        f"📍Free VIP : @VIPLiveRecords\n"
+    )
+    if thumb:
+        try:
+            logo = thumb
+            await lsid.delete()
+            msg = await bot.send_file(lsid.chat_id, logo, caption=output)
+            await asyncio.sleep(300)
+            # await msg.delete()
+        except BaseException:
+            await lsid.edit(
+                output + "\n\n ***Logo yang diberikan tidak valid."
+                "\nPastikan link diarahkan ke gambar logo**"
+            )
+            # await asyncio.sleep(100)
+            # await lsid.delete()
+    else:
+        await edit_or_reply(lsid, output)
         
         
 CMD_HELP.update(

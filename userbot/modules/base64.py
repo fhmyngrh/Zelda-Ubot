2fad23f0d4dfa12a8dd556b7513b984170b8bd12
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-17 18:17:43 +0700

    Create base64.py

---
+++ userbot/modules/base64.py
@@ -0,0 +1,40 @@
+import pybase64
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
+from userbot.events import zelda_cmd, edit_delete, edit_or_reply
+
+@bot.on(zelda_cmd(outgoing=True, pattern=r"enc(?: |$)(.*)"))
+async def endecrypt(query):
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+    lething = str(pybase64.b64encode(bytes(msg, "utf-8")))[2:]
+    await query.edit("`" + lething[:-1] + "`")
+    
+        
+@bot.on(zelda_cmd(outgoing=True, pattern=r"dec(?: |$)(.*)"))
+async def endecrypt(query):
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        return await edit_delete(event, "**Berikan Sebuah Pesan atau Reply**")
+    lething = str(pybase64.b64decode(bytes(msg, "utf-8"), validate=True))[2:]
+    await query.edit(f"**Decoded from** `{msg}` **:**\n`" + lething[:-1] + "`")
+        
+
+CMD_HELP.update(
+    {
+        "encrypt": f"**Plugin : **`base64`\
+        \n\n  •  **Syntax :** `{cmd}enc`\
+        \n  •  **Function : **Enkripsi pesan dengan base64.**\
+        \n\n  •  **Syntax :** `{cmd}dec`\
+        \n  •  **Function : **Mentranslate dari Kode base64.**\
+    "
+    }
+)
